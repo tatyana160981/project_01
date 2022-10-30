c18c1b5bd300ebbe7df55dd6331e55f082b42c49
tatyana160981 <115876356+tatyana160981@users.noreply.github.com>
2022-10-30 21:54:51 +0300

    Add files via upload

---
+++ task_01 DZ Tazitdinova.py
@@ -0,0 +1,19 @@
+# Есть строка с перечислением песен
+
+my_favorite_songs = ["Waste a Moment", "Staying\' Alive", "A Sorta Fairytale", "Start Me Up", "New Salvation"]
+
+print(my_favorite_songs[0])
+print(my_favorite_songs[3])
+print(my_favorite_songs[4])
+
+
+
+
+
+
+
+
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
